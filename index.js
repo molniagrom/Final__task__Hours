664e10ff55670a5471b16d87fa24b3611f94ee09
molnia_grom <mgrom250@gmail.com>
2024-10-22 13:26:55 +0300

    Attempt 1

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 let timerId; // переменная, которая будет хранить ID таймера
 
-
-const startButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
-startButton.ЭДэВЕНТлИСТНЕР('ТЯП', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-    timerId = СЕТиНТРЕВАЛ(updateClock, ОДНАсЕКУНДА); // запускаем  updateClock() каждую секунду
-});
-
-
-const stopButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
-stopButton.addEventListener('ТЯП', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-    clearInterval(timerId); // останавливаем таймер
-});
-
-
 function updateClock() {
-    const clock = document.getElementById('clock');
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
-    const now = НОВАЯдАТА();
-    const hours = now.ДАЙчАСЫ()
-    const minutes = now.ДАЙмИНУТЫ()
-    const seconds = now.ДАЙсЕКУНДЫ()
-    clock.textContent = hours + ':' + minutes + ':' + seconds;
+ // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document
+    .getElementById("clock")
+    .textContent(`${hours}:${minutes}:${seconds}`);
 }
+
+const startButton = document.getElementById("start");
+startButton.addEventListener("click", function () {
+  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
+  timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+});
+
+const stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", function () {
+  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
+  clearInterval(timerId); // останавливаем таймер
+});
+
+
